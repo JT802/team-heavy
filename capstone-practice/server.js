680cf7258d2e35da4332ddc0db7bf364e743836f
MillerTime802 <70776435+MillerTime802@users.noreply.github.com>
2020-11-12 18:30:36 -0500

    omeka pics

--- capstone-practice/server.js
+++ capstone-practice/server.js
@@ -8,18 +8,18 @@ const port = process.env.PORT || 8000;
 app.use(express.static("./public"));
 
 app.get("/", function (req, res, next) {
-  request(
-    "https://omeka.bigheavyworld.com/api/items=7",
+request({
+  uri: "https://omeka.bigheavyworld.com/api/items?collection=7",
 
-    function (error, response, body) {
-      if (!error && response.statusCode === 200) {
-        console.log(body);
-        res.json(body);
-      } else {
-        res.json(error);
-      }
-    }
-  );
+ function(error, response, body) {
+   if (!error && response.statusCode === 200) {
+    console.log(body);
+    res.json(body);
+  } else {
+    res.json(error);
+  }
+  },
+  });
 });
 app.listen(port, () => {
   console.log("running on port: " + port);
