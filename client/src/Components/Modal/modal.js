801e19c00088e58f209a86d696ccbaa8a412cf5e
brendan802 <67173637+brendan802@users.noreply.github.com>
2020-11-13 08:55:45 -0500

    modalpractice

---
+++ client/src/Components/Modal/modal.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+
+const Container = styled.div`
+    display: flex;
+    justify-content: center;
+
+`
+export default function App() {
+return (
+<>
+<h1>hello</h1>
+</>
+
+
+)
+
+
+}
