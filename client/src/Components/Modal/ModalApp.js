a8e735c9a5ab514d002d1e264cfe0f222a3de8d1
MillerTime802 <70776435+MillerTime802@users.noreply.github.com>
2020-11-19 19:32:28 -0500

    no M

--- client/src/Components/Modal/ModalApp.js
+++ client/src/Components/Modal/ModalApp.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Modal } from "./Modal";
+import { Modal } from "./odal";
 import styled from "styled-components";
 
 /*About the Photographer Button*/
