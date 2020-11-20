a4aa23ef0e849296ddd56fc896488e24ef4e7218
JT802 <jonathan.tresch@gmail.com>
2020-11-20 15:07:45 -0500

    fix (CSS): fixed NavBar (burger) & About

--- client/src/Components/NavBarComponent/NavBar.js
+++ client/src/Components/NavBarComponent/NavBar.js
@@ -1,12 +1,13 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { FcMenu } from "react-icons/fc";
 import "./NavBar.css";
 
 function NavBar() {
     const [dropDown, setDropDown] = useState("none")
 
     function dropdownHandler() {
-        if(dropDown === "none") {
+        if (dropDown === "none") {
             setDropDown(true)
         } else {
             setDropDown("none")
@@ -14,17 +15,16 @@ function NavBar() {
     }
 
     return (
-        // -- Some of these paths are temporary until other components are updated. -- //
         // -- It's simpler to make this a persistent dropdown menu instead of a horizontal bar. -- //
         <nav className="navigation" id="dropdown">
-            <span onClick={dropdownHandler} id="learnMore">Learn More</span>
+            <span onClick={dropdownHandler} ><FcMenu id="burger" /></span>
             {/*Using Link to add dropdwon menu items to components  */}
-            <div className="menu-content" style={{display: dropDown}}>
-                <li><Link className="navText" to="/">Go Home</Link></li>
-                <li><Link className="navText" to="/About">About 242 Main</Link></li>
-                <li><Link className="navText" to="/DeckCascade">Concert Photos by Tim Snow</Link></li>
-                <li><Link className="navText" to="/HorizontalCarousel">Concert Posters</Link></li>
-                <li><Link className="navText" to="/BrentCarousel">Interviews</Link></li>
+            <div className="menu-content" style={{ display: dropDown }}>
+                <li onClick={dropdownHandler}><Link className="navText" to="/">Go Home</Link></li>
+                <li onClick={dropdownHandler}><Link className="navText" to="/About">About 242 Main</Link></li>
+                <li onClick={dropdownHandler}><Link className="navText" to="/DeckCascade">Concert Photos by Tim Snow</Link></li>
+                <li onClick={dropdownHandler}><Link className="navText" to="/HorizontalCarousel">Concert Posters</Link></li>
+                <li onClick={dropdownHandler}><Link className="navText" to="/BrentCarousel">Interviews</Link></li>
                 {/* The "About" text will change when Jim sends us the specifics. */}
             </div>
         </nav>
