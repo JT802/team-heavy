9a11a3484edd4b998dcb7541ade177a812a4b2a5
JT802 <jonathan.tresch@gmail.com>
2020-11-24 10:44:25 -0500

    fix(CSS, comments): Fixed CSS and added comments on Info

--- client/src/Components/InformationComponent/InfoLink.js
+++ client/src/Components/InformationComponent/InfoLink.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 import { BsInfoSquare } from "react-icons/bs";
 
 function InfoLink() {
-
+    //The InfoLink persists across all pages, but only navigates to the Information page.
     return (
         <Link to="/Information"><BsInfoSquare id="info-link"/></Link>
     )
