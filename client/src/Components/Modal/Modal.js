4010cf993bfebf0bfd3b26d3b077cf20efd16d04
Dfaye023 <70286830+Dfaye023@users.noreply.github.com>
2020-11-30 15:02:23 -0500

    removed unused image files

--- client/src/Components/Modal/Modal.js
+++ client/src/Components/Modal/Modal.js
@@ -1,15 +1,15 @@
-import React, { useRef, useEffect, useCallback} from "react";
-import {useSpring, animated} from  'react-spring'
-import "../../images/Tim-Snow.jpg"
+import React, { useRef, useEffect, useCallback } from "react";
+import { useSpring, animated } from "react-spring";
+import "../../images/Tim-Snow.jpg";
 import styled from "styled-components";
-import {AiOutlineClose} from "react-icons/ai"
+import { AiOutlineClose } from "react-icons/ai";
 
 //styles for components
 const Background = styled.div`
   position: absolute;
   width: 100%;
   height: 65vh;
-  z-index:10;
+  z-index: 10;
   background: rgba(0, 0, 0, 0.8);
   display: flex;
 `;
@@ -42,65 +42,74 @@ const ModalContent = styled.div`
 const Text = styled.p`
   font-size: 1.8rem;
   color: gold;
-  max-width:120%;
+  max-width: 120%;
 `;
 const Name = styled.h1`
   font-size: 3.5rem;
   color: gold;
-  margin-left:1vw;
-  margin-top:-5vh;
+  margin-left: 1vw;
+  margin-top: -5vh;
 `;
 
 //Not using this pop down animation but when I comment it out it breaks my code so leaving for now, no effect might come back to it
 export const Modal = ({ showModal, setShowModal }) => {
-    const modalRef = useRef()
-//animation with use Spring for model, 250 ms duration
-const animation = useSpring({
+  const modalRef = useRef();
+  //animation with use Spring for model, 250 ms duration
+  const animation = useSpring({
     config: {
-    //duration of fall animatiom
-    duration: 550
-    },//translates from top of screen out 100%
+      //duration of fall animatiom
+      duration: 550,
+    }, //translates from top of screen out 100%
     opacity: showModal ? 1 : 0,
-    transform: showModal ? `translateY(0%)` : `translateY(-100%)`
-});
+    transform: showModal ? `translateY(0%)` : `translateY(-100%)`,
+  });
 
+  const keyPress = useCallback(
+    (e) => {
+      //if the escape key is pressed and the modal is open/ set show modal to false(close Modal)
+      if (e.key === "Escape" && showModal) {
+        setShowModal(false);
+      }
+    },
+    [setShowModal, showModal]
+  );
 
-const keyPress = useCallback (e => {
-  //if the escape key is pressed and the modal is open/ set show modal to false(close Modal)
-  if(e.key === 'Escape' && showModal){
-    setShowModal(false)
-  }
-}, [setShowModal, showModal])
-
-//calling keyPress function from above
-useEffect(() => {
-  document.addEventListener('keydown', keyPress );
-  //remove the event listener 
-  return() => document.removeEventListener('keydown', keyPress)
-}, [keyPress])
+  //calling keyPress function from above
+  useEffect(() => {
+    document.addEventListener("keydown", keyPress);
+    //remove the event listener
+    return () => document.removeEventListener("keydown", keyPress);
+  }, [keyPress]);
 
   return (
     <>
       {/*If show modal, then render rest of the content */}
       {showModal ? (
         //if background clicked modal closes-onClick = {closeModal}
-        <Background ref = {modalRef}>
-          <animated.div style = {animation}>
-          <ModalWrapper showModal={showModal}>
-            <CloseButton
+        <Background ref={modalRef}>
+          <animated.div style={animation}>
+            <ModalWrapper showModal={showModal}>
+              <CloseButton
                 aria-label="close modal"
                 onClick={() => setShowModal((prev) => !prev)}
               ></CloseButton>
               <ModalContent>
                 <Name>Tim Snow </Name>
                 <Text>
-              Tim Snow is a Montreal based photojournalist and photography educator. He took these images at the “final” 242 Main concert on December 3, 2016. 
-              242 Main is located in the basement of Memorial Auditorium; when the building became unsafe, the city closed 242 Main and other programs. 22 bands performed at this concert to celebrate 242 Main, and Big Heavy World began the years-long advocacy that would ensure youth-led music would emerge in the space again. Find more of Tim's images at timsnowphotography.com. 
-              Tim can be contacted by email at tim@timsnowphotography.com or at 514-238-4529.
+                  Tim Snow is a Montreal based photojournalist and photography
+                  educator. He took these images at the “final” 242 Main concert
+                  on December 3, 2016. 242 Main is located in the basement of
+                  Memorial Auditorium; when the building became unsafe, the city
+                  closed 242 Main and other programs. 22 bands performed at this
+                  concert to celebrate 242 Main, and Big Heavy World began the
+                  years-long advocacy that would ensure youth-led music would
+                  emerge in the space again. Find more of Tim's images at
+                  timsnowphotography.com. Tim can be contacted by email at
+                  tim@timsnowphotography.com or at 514-238-4529.
                 </Text>
               </ModalContent>
-           {/* </ModalImage> */}
-          </ModalWrapper>
+              {/* </ModalImage> */}
+            </ModalWrapper>
           </animated.div>
         </Background>
       ) : null}
